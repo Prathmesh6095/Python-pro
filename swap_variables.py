0cea2706f81e05e4582f956abece1a1db0816d2a
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-22 12:51:59 +0530

    Swaping variables using python

---
+++ swap_variables.py
@@ -0,0 +1,23 @@
+# WAP to Swap two variables
+# Method 1
+x = 13;
+y = 12;
+
+temp = x;
+print("Value of temp variable is", temp)
+
+x = y;
+print("Value of x is", x)
+
+y = temp
+print("The value of y is", y,"\n",)
+
+print("The second method is: ")
+# Method 2 - Without using third variable
+x = 12
+y = 13
+
+x,y = y,x;
+
+print("The value of x", x)
+print("The value of y", y)
