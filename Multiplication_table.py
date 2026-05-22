fd0e4e47ff8b1206aff4ab68389eb46bad441e10
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-22 14:37:24 +0530

    Multiplication table

---
+++ Multiplication_table.py
@@ -0,0 +1,15 @@
+# Python program to display the multiplication table
+
+# n = int(input("Enter a number: "))
+
+# for i in range (1, 11):
+#     print(f"{n} X {i} = {n*i}")
+
+print("\n Using while")
+n = 5
+i = 1
+while(i<=10):
+    print(f"{n} X {i} = {n*i}")
+    i=i+1
+    
+    
