54c00c418685e596de19c5f6f1c01b6590b421dd
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-23 13:35:23 +0530

    Using Math module

---
+++ squareroot.py
@@ -0,0 +1,15 @@
+# Python program to find the Square Root
+
+# Solution - 1(Using Exponentiation)
+
+import math
+
+# num1 = int(input("Enter a number: "))
+# sqr = num1**(0.5)
+# print(f"Square root of {num1} is {sqr}")
+
+
+# Solution - 2
+num1 = int(input("Enter a number: "))
+sr = math.sqrt(num1)
+print(sr)
