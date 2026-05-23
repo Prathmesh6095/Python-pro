01e3b1685d7d5875a36042166953a90033fc290f
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-23 13:48:06 +0530

    Checking Armstrong number

---
+++ Check_Armstrong.py
@@ -0,0 +1,18 @@
+# Check number is armstrong or not?
+
+n = int(input("Enter a number more than two digit : "))
+
+sum = 0 
+temp = n
+
+while temp > 0:
+    digit = temp % 10
+    cube = digit ** 3
+    sum = sum + cube
+    temp //= 10
+
+if sum == n:
+    print("It's  an Armstrong number.")
+
+else:
+    print("It's not Armstrong number.")
