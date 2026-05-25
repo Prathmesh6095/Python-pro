7c50f80156ae07c08445ecb071053b9aecfeabb9
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-25 08:57:47 +0530

    Practice

--- practice.py
+++ practice.py
@@ -1,15 +1,12 @@
-x = 0
-y = 1
+# Check the number is prime or not.
+n = int(input("Enter a number: "))
 
-n = int(input(" Enter a number: "))
-
-if n == 1 or n<1:
-    print(x)
-else:
-    print(x)
-    print(y)
+if n ==1:
+    print("invalid choice.")
+if n > 1:
     for i in range(2, n):
-        z = x + y
-        x = y
-        y = z
-        print(z)
+        if n % i == 0:
+            print("Not prime.")
+            break
+    else:
+            print("It's prime.")
