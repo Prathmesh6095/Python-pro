a473231570d39e069bbaf27192679baefc3bb3d4
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-26 11:39:14 +0530

    Practice facto

--- practice.py
+++ practice.py
@@ -1,12 +1,9 @@
-# Check the number is prime or not.
-n = int(input("Enter a number: "))
+# Python program to find the factorial of number
 
-if n ==1:
-    print("invalid choice.")
-if n > 1:
-    for i in range(2, n):
-        if n % i == 0:
-            print("Not prime.")
-            break
-    else:
-            print("It's prime.")
+n = int(input("Enter a number: "))
+facto = 1
+for i in range(1, n+1):
+    facto=facto*i
+
+print(facto)
+    
