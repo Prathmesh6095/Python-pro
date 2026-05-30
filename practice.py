46b41eed8e0b475f2519a75176a6f4fb6d8a441d
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-30 16:56:11 +0530

    Practice

--- practice.py
+++ practice.py
@@ -1,21 +1,5 @@
-x = 13;
-y = 12;
+import math
 
-temp = x;
-print("Value of temp variable is", temp)
-
-x = y;
-print("Value of x is", x)
-
-y = temp
-print("The value of y is", y,"\n",)
-
-print("The second method is: ")
-# Method 2 - Without using third variable
-x = 12
-y = 13
-
-x,y = y,x;
-
-print("The value of x", x)
-print("The value of y", y)
+num1 = int(input("Enter a number: "))
+sr = math.sqrt(num1)
+print(sr)
