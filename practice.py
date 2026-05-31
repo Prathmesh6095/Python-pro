46e4bde60b02f87214572d3f21b34a4c97db60c5
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-05-31 12:30:43 +0530

    Practice

--- practice.py
+++ practice.py
@@ -1,5 +1,14 @@
-import math
+n1 = 0
+n2 = 1
+n = int(input("Enter a number: "))
 
-num1 = int(input("Enter a number: "))
-sr = math.sqrt(num1)
-print(sr)
+if n == 1 and n < 1:
+    print(n1)
+else:
+    print(n1)
+    print(n2)
+    for i in range(2, n):
+         c = n1 + n2
+         n1 = n2
+         n2 = c
+         print(c)
