b0b96e345e85a2ecb4efba66cd81caac824dbd07
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-06-18 14:36:08 +0530

    Practice

--- Practice/Q1.py
+++ Practice/Q1.py
@@ -1,16 +1,5 @@
-n = int(input("Enter a more than two digit: "))
+km = int(input("Enter dist in km: "))
 
-sum = 0
-temp = n
+miles = km*(0.621371)
 
-while temp > 0:
-    digit = temp % 10
-    cube = digit ** 3
-    sum = sum + cube
-    temp //= 10
-
-if sum == n:
-    print("It's an armstrong.")
-
-else:
-    print("It's not armstrong.")
+print(miles)
