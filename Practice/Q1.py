083d3ee87fe02bf19d72e09b0b1d1acd483b7728
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-06-18 14:49:31 +0530

    Practice

--- Practice/Q1.py
+++ Practice/Q1.py
@@ -1,7 +1,15 @@
+a = 0
+b = 1
 n = int(input("Enter a number: "))
 
-facto = 1
-for i in range(1, n+1):
-    facto=facto*i
-    
-print(facto)
+if n == 1 or n < 0:
+    print(a)
+
+else:
+    print(a)
+    print(b)
+    for i in range(2,n):
+        c = a + b
+        a = b
+        b = c
+        print(c)
