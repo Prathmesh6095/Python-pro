731e3716f91b49daea0878553133925592a5fce9
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-06-18 13:10:40 +0530

    Practice

--- Practice/Q1.py
+++ Practice/Q1.py
@@ -1,4 +1,16 @@
-h = float(input("Enter a height: "))
-b = float(input("Enter a base: "))
+n = int(input("Enter a more than two digit: "))
 
-print("area =",(0.5)*h*b)
+sum = 0
+temp = n
+
+while temp > 0:
+    digit = temp % 10
+    cube = digit ** 3
+    sum = sum + cube
+    temp //= 10
+
+if sum == n:
+    print("It's an armstrong.")
+
+else:
+    print("It's not armstrong.")
