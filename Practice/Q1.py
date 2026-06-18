6b89c781e3fc64bdeff1706b2a3318f3d9ab0789
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-06-18 15:02:42 +0530

    Practice

--- Practice/Q1.py
+++ Practice/Q1.py
@@ -1,14 +1,6 @@
+import math as m
 n = int(input("Enter a number: "))
 
-if n==1:
-    print("Invalid choice")
+sr = m.sqrt(n)
+print(sr)
 
-elif n>1:
-    for i in range(2, n):
-        if n%i==0:
-            print("Not prime ")
-            break
-
-    else:
-        print("It's Prime")
-        
