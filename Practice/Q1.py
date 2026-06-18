34d06af380380424acca324701d3ede94a165bcb
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-06-18 14:54:34 +0530

    Practice year

--- Practice/Q1.py
+++ Practice/Q1.py
@@ -1,10 +1,4 @@
-yr = int(input("Enter a year: "))
+n = int(input("Enter a number: "))
 
-if (yr % 400 == 0) and (yr % 100 == 0):
-    print("It's  leap year")
-
-elif (yr % 4 == 0) and (yr % 100 != 0):
-    print("It's leap year")
-
-else:
-    print("It's  not leap year.")
+for i in range(1, 11):
+    print(n*i)
