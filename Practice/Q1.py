87d98269708700c7e57c416e7d952044517bb40e
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-06-18 15:00:10 +0530

    Practice year

--- Practice/Q1.py
+++ Practice/Q1.py
@@ -1,4 +1,14 @@
 n = int(input("Enter a number: "))
 
-for i in range(1, 11):
-    print(n*i)
+if n==1:
+    print("Invalid choice")
+
+elif n>1:
+    for i in range(2, n):
+        if n%i==0:
+            print("Not prime")
+            break
+
+    else:
+        print("It's Prime")
+        
