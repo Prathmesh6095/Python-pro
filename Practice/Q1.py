9278edcbb7b8f1526a641a8e6cfcae08318c866f
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-06-19 12:17:31 +0530

    Practice list

--- Practice/Q1.py
+++ Practice/Q1.py
@@ -1,7 +1,9 @@
-letter = '''
-        Hii <|Name|>
-        I'm Kai, Wanna meet you!
-        come at <|Location|>
-        '''
+l1 = ["Patu",3,25,365,"Rohan"]
 
-print(letter.replace("<|Name|>","Prathamesh").replace("<|Location|>","BeyStadium."))
+print(l1.count(3))
+print(l1.index(365))
+# print(l1.pop(3))
+
+
+print(l1.insert(3,"3"))
+print(l1)
