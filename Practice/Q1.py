e0bf7c156764b3dfd02aa912672306b5ab4a0328
Prathamesh hatkar <prathameshhatkar07@gmail.com>
2026-06-19 12:45:54 +0530

    Practice list

--- Practice/Q1.py
+++ Practice/Q1.py
@@ -1,16 +1,4 @@
-marks = []
+l = [2,8,5,9,7]
 
-m1 = int(input("Enter the marks: "))
-marks.append(m1)
-
-m2 = int(input("Enter the marks :"))
-marks.append(m2)
-m3 = int(input("Enter the marks :"))
-marks.append(m3)
-m4 = int(input("Enter the marks :"))
-marks.append(m4)
-m5 = int(input("Enter the marks :"))
-marks.append(m5)
-
-marks.sort()
-print(marks)
+print(sum(l))
+print(l.count(8))
